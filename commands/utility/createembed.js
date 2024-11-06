c11d0548b7b6823167e31fd5208f4dabfc2f44bd
Atixshay <atishayprasad184@gmail.com>
2024-11-06 21:06:04 +0530

    Update createembed.js

--- commands/utility/createembed.js
+++ commands/utility/createembed.js
@@ -125,7 +125,7 @@ module.exports = {
         .setAuthor({ 
           name: lang.createembedAlertTitle, 
           iconURL: cmdIcons.dotIcon ,
-          url: "https://discord.gg/xQF9f9yUEM"
+          url: "https://discord.gg/1kiss"
         })
         .setDescription(lang.createembedAlertDescription)
         .setTimestamp();
