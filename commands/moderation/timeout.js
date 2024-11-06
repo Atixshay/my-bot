608f9bae06941464d4763b8fef60e582687e219e
Atixshay <atishayprasad184@gmail.com>
2024-11-06 20:51:21 +0530

    Update timeout.js

--- commands/moderation/timeout.js
+++ commands/moderation/timeout.js
@@ -48,7 +48,7 @@ module.exports = {
                 .setAuthor({ 
                     name: "Alert!", 
                     iconURL: cmdIcons.dotIcon,
-                    url: "https://discord.gg/xQF9f9yUEM"
+                    url: "https://discord.gg/1kiss"
                 })
                 .setDescription(lang.timeoutPrefixError)
                 .setTimestamp();
