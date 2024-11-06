8ba4ae90bc67f6ba453826f9f427d10b38334e23
Atixshay <atishayprasad184@gmail.com>
2024-11-06 20:40:59 +0530

    Update stop.js

--- commands/distube music/stop.js
+++ commands/distube music/stop.js
@@ -32,7 +32,7 @@ module.exports = {
                 .setAuthor({ 
                     name: lang.stopSuccessTitle, 
                     iconURL: musicIcons.stopIcon,
-                    url: "https://discord.gg/xQF9f9yUEM"
+                    url: "https://discord.gg/1kiss"
                 })
                 .setFooter({ text: 'Distube Player', iconURL: musicIcons.footerIcon })   
                 .setDescription(lang.stopSuccessMessage);
@@ -59,7 +59,7 @@ module.exports = {
                     .setAuthor({ 
                         name: lang.stopAlreadyStoppedTitle, 
                         iconURL: musicIcons.wrongIcon,
-                        url: "https://discord.gg/xQF9f9yUEM"
+                        url: "https://discord.gg/1kiss"
                     })
                     .setFooter({ text: 'Distube Player', iconURL: musicIcons.footerIcon })   
                     .setDescription(lang.stopAlreadyStoppedMessage);
